2f7081d53a748d0a3c8bd92ee6b63d5a0216c967
as3boyan <denis.flash.as3@gmail.com>
2014-01-26 17:00:59 +0200

    DomainManager and AppInit

---
+++ src/brackets/utils/AppInit.hx
@@ -0,0 +1,18 @@
+package brackets.utils;
+
+/**
+ * ...
+ * @author AS3Boyan
+ */
+@:native("brackets_externs.utils.AppInit")
+extern class AppInit
+{
+
+	public function new() 
+	{
+		
+	}
+	
+	public function appReady(onReady:Dynamic):Void;
+	
+}
